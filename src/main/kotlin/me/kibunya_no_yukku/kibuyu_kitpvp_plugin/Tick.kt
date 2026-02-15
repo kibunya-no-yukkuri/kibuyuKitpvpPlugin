af1e193ed64886e5525ff3c065a9d5ccf21d8f53
kibunya_no_yukkuri <pentomgz28@gmail.com>
2026-02-15 23:29:43 +0900

    test

--- src/main/kotlin/me/kibunya_no_yukku/kibuyu_kitpvp_plugin/Tick.kt
+++ src/main/kotlin/me/kibunya_no_yukku/kibuyu_kitpvp_plugin/Tick.kt
@@ -6,7 +6,6 @@ import net.md_5.bungee.api.ChatMessageType
 import net.md_5.bungee.api.chat.TextComponent
 import org.bukkit.*
 import org.bukkit.entity.Player
-import org.bukkit.plugin.Plugin
 import org.bukkit.plugin.java.JavaPlugin
 import org.bukkit.scheduler.BukkitRunnable
 import kotlin.math.cos
